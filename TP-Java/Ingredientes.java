cb87ab81cd38363d73b3aa6408e589c98dbfc5ac
KellyJJJ <kellydcjb@gmail.com>
2024-11-22 21:05:54 -0300

    Ingredientes.java

---
+++ TP-Java/Ingredientes.java
@@ -0,0 +1,17 @@
+class Ingrediente {
+    private String nombre;
+    private int precio;
+
+    public Ingrediente(String nombre, int precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+}
